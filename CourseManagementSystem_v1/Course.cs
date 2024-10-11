93b88e259adad2f163527ae02d98a30ff552b6e0
UT01138 <UT01138@UNICOMTIC12>
2024-10-11 14:07:57 +0530

    Add project files.

---
+++ CourseManagementSystem_v1/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v1
+{
+    public class Course
+    {
+        private string courseId;
+        private string title;
+        private string Duration;
+        private decimal Price;
+
+
+    }
+}
